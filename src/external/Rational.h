1de6f72c0bb948a9375e51b0d480bf418d2b3da7
Tristan Schulz <mrdiverlp@gmail.com>
2024-03-14 06:56:38 +0100

    Migrated from gmp to boost/multiprecision

--- src/external/Rational.h
+++ src/external/Rational.h
@@ -8,168 +8,147 @@
 
 #ifndef TRIWILD_RATIONAL_H
 #define TRIWILD_RATIONAL_H
-
-#include <gmp.h>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <iostream>
+using namespace boost::multiprecision;
 
 namespace triwild {
 
-    class Rational {//https://cs.nyu.edu/acsys/cvc3/releases/1.5/doc/rational-gmp_8cpp-source.html
-    public:
-        mpq_t value;
-        void canonicalize() {
-            mpq_canonicalize(value);
+class Rational
+{  // https://cs.nyu.edu/acsys/cvc3/releases/1.5/doc/rational-gmp_8cpp-source.html
+  public:
+    cpp_rational value;
+    void         canonicalize() {}
+    int          get_sign() { return (value > 0) * 1 + (value < 0) * -1; }
+
+    Rational()
+        : value(0)
+    {}
+
+    Rational(double d)
+        : value(d)
+    {}
+
+    Rational(const cpp_rational& v_)
+        : value(v_)
+    {}
+
+    Rational(const Rational& other)
+        : value(other.value)
+    {}
+
+    ~Rational() { value = 0; }
+
+    //        //+, - another point
+    //        Rational operator+(const Rational &r) const {
+    //            Rational r_out;
+    //            mpq_add(r_out.value, value, r.value);
+    //            return r_out;
+    //        }
+    //
+    //        Rational operator-(const Rational &r) const {
+    //            Rational r_out;
+    //            mpq_sub(r_out.value, value, r.value);
+    //            return r_out;
+    //        }
+    //
+    //        //*, / double/rational
+    //        Rational operator*(const Rational &r) const {
+    //            Rational r_out;
+    //            mpq_mul(r_out.value, value, r.value);
+    //            return r_out;
+    //        }
+    //
+    //        Rational operator/(const Rational &r) const {
+    //            Rational r_out;
+    //            mpq_div(r_out.value, value, r.value);
+    //            return r_out;
+    //        }
+    //
+    //        //=
+    //        void operator=(const Rational &r) {
+    //            mpq_set(value, r.value);
+    //        }
+
+    friend Rational operator-(const Rational& x)
+    {
+        Rational r_out(-x.value);
+        return r_out;
+    }
+
+    friend Rational operator+(const Rational& x, const Rational& y)
+    {
+        Rational r_out(x.value + y.value);
+        return r_out;
+    }
+
+    friend Rational operator-(const Rational& x, const Rational& y)
+    {
+        Rational r_out(x.value - y.value);
+        return r_out;
+    }
+
+    friend Rational operator*(const Rational& x, const Rational& y)
+    {
+        Rational r_out(x.value * y.value);
+        return r_out;
+    }
+
+    friend Rational operator/(const Rational& x, const Rational& y)
+    {
+        Rational r_out(x.value / y.value);
+        return r_out;
+    }
+
+    friend Rational pow(const Rational& x, int p)
+    {
+        Rational r_out = x;
+        for (int i = 1; i < std::abs(p); i++) {
+            r_out = r_out * x;
         }
-        int get_sign() {
-            return mpq_sgn(value);
-        }
+        if (p < 0)
+            return 1 / r_out;
+        return r_out;
+    }
 
-        Rational() {
-            mpq_init(value);
-            mpq_set_d(value, 0);
-        }
-
-        Rational(double d) {
-            mpq_init(value);
-            mpq_set_d(value, d);
-//            canonicalize();
-        }
-
-        Rational(const mpq_t &v_) {
-            mpq_init(value);
-            mpq_set(value, v_);
-//            canonicalize();
-        }
-
-        Rational(const Rational &other) {
-            mpq_init(value);
-            mpq_set(value, other.value);
-        }
-
-        ~Rational() {
-            mpq_clear(value);
-        }
-
-//        //+, - another point
-//        Rational operator+(const Rational &r) const {
-//            Rational r_out;
-//            mpq_add(r_out.value, value, r.value);
-//            return r_out;
-//        }
-//
-//        Rational operator-(const Rational &r) const {
-//            Rational r_out;
-//            mpq_sub(r_out.value, value, r.value);
-//            return r_out;
-//        }
-//
-//        //*, / double/rational
-//        Rational operator*(const Rational &r) const {
-//            Rational r_out;
-//            mpq_mul(r_out.value, value, r.value);
-//            return r_out;
-//        }
-//
-//        Rational operator/(const Rational &r) const {
-//            Rational r_out;
-//            mpq_div(r_out.value, value, r.value);
-//            return r_out;
-//        }
-//
-//        //=
-//        void operator=(const Rational &r) {
-//            mpq_set(value, r.value);
-//        }
-
-
-        friend Rational operator-(const Rational& x) {
-            Rational r_out;
-            mpq_neg(r_out.value, x.value);
-            return r_out;
-        }
-
-        friend Rational operator+(const Rational& x, const Rational& y) {
-            Rational r_out;
-            mpq_add(r_out.value, x.value, y.value);
-            return r_out;
-        }
-
-        friend Rational operator-(const Rational& x, const Rational& y) {
-            Rational r_out;
-            mpq_sub(r_out.value, x.value, y.value);
-            return r_out;
-        }
-
-        friend Rational operator*(const Rational& x, const Rational& y) {
-            Rational r_out;
-            mpq_mul(r_out.value, x.value, y.value);
-            return r_out;
-        }
-
-        friend Rational operator/(const Rational& x, const Rational& y) {
-            Rational r_out;
-            mpq_div(r_out.value, x.value, y.value);
-            return r_out;
-        }
-
-        friend Rational pow(const Rational& x, int p) {
-            Rational r_out = x;
-            for (int i = 1; i < std::abs(p); i++) {
-                r_out = r_out * x;
-            }
-            if (p < 0)
-                return 1 / r_out;
-            return r_out;
-        }
-
-        Rational& operator=(const Rational& x) {
-            if (this == &x) return *this;
-            mpq_set(value, x.value);
+    Rational& operator=(const Rational& x)
+    {
+        if (this == &x)
             return *this;
-        }
+        value = x.value;
+        return *this;
+    }
 
-        Rational& operator=(const double x) {
-            mpq_set_d(value, x);
-//            canonicalize();
-            return *this;
-        }
+    Rational& operator=(const double x)
+    {
+        value = x;
+        //            canonicalize();
+        return *this;
+    }
 
-        //> < ==
-        friend bool operator<(const Rational &r, const Rational &r1) {
-            return mpq_cmp(r.value, r1.value) < 0;
-        }
+    //> < ==
+    friend bool operator<(const Rational& r, const Rational& r1) { return r.value < r1.value; }
 
-        friend bool operator>(const Rational &r, const Rational &r1) {
-            return mpq_cmp(r.value, r1.value) > 0;
-        }
+    friend bool operator>(const Rational& r, const Rational& r1) { return r.value > r1.value; }
 
-        friend bool operator<=(const Rational &r, const Rational &r1) {
-            return mpq_cmp(r.value, r1.value) <= 0;
-        }
+    friend bool operator<=(const Rational& r, const Rational& r1) { return r.value <= r1.value; }
 
-        friend bool operator>=(const Rational &r, const Rational &r1) {
-            return mpq_cmp(r.value, r1.value) >= 0;
-        }
+    friend bool operator>=(const Rational& r, const Rational& r1) { return r.value >= r1.value; }
 
-        friend bool operator==(const Rational &r, const Rational &r1) {
-            return mpq_equal(r.value, r1.value);
-        }
+    friend bool operator==(const Rational& r, const Rational& r1) { return r.value == r1.value; }
 
-        friend bool operator!=(const Rational &r, const Rational &r1) {
-            return !mpq_equal(r.value, r1.value);
-        }
+    friend bool operator!=(const Rational& r, const Rational& r1) { return !(r.value == r1.value); }
 
-        //to double
-        double to_double() {
-            return mpq_get_d(value);
-        }
+    // to double
+    double to_double() { return value.convert_to<double>(); }
 
-        //<<
-        friend std::ostream &operator<<(std::ostream &os, const Rational &r) {
-            os << mpq_get_d(r.value);
-            return os;
-        }
-    };
-}
+    //<<
+    friend std::ostream& operator<<(std::ostream& os, const Rational& r)
+    {
+        os << r.value;
+        return os;
+    }
+};
+}  // namespace triwild
 
-#endif //TRIWILD_RATIONAL_H
+#endif  // TRIWILD_RATIONAL_H
